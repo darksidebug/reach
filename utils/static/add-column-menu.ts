2345a762f2fcbadbd19efc9cc32f3fc57c56909d
darksidebug <nobeginmasob@gmail.com>
2023-06-26 20:34:54 -0700

    Update: import links

--- utils/static/add-column-menu.ts
+++ utils/static/add-column-menu.ts
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic';
 export const addColumnMenu = [
   {
     text: 'to the right',
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     src: dynamic(() => import('@/icons/arrow-right.svg'), { ssr: false }),
   }
 ]
--- utils/static/column-menu.ts
+++ utils/static/column-menu.ts
@@ -6,8 +6,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/move-column'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/move-column'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
@@ -15,8 +15,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/merge-cell'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/merge-cell'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
@@ -24,8 +24,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/align-content'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/align-content'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
@@ -33,8 +33,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/add-column'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/add-column'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
@@ -42,8 +42,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/add-row'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/add-row'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
@@ -51,8 +51,8 @@ export const columnMenu = [
     append: true,
     hasSubMenu: true,
     hasIcon: true,
-    component: dynamic(() => import('@/components/common/submenu/sort'), { ssr: false }),
-    icon: dynamic(() => import('@/components/common/icon/icon'), { ssr: false }),
+    component: dynamic(() => import('@/app/components/common/submenu/sort'), { ssr: false }),
+    icon: dynamic(() => import('@/app/components/common/icon/icon'), { ssr: false }),
     iconClass: 'relative h-2.5 w-auto transition-transform'
   },
   {
